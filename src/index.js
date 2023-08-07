5970a67fa0377792a4a53df3cc8f002100be0f68
Mr Guu <pyaetheta@gmail.com>
2023-08-07 09:56:17 +0630

    move footer into browser router

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ root.render(
         <Route path="/Fund" element={<Fund />} />
         <Route path="/Payout" element={<Payout />} />
       </Routes>
+      {<Footer />}
     </BrowserRouter>
-    {<Footer />}
   </React.StrictMode>
 );
